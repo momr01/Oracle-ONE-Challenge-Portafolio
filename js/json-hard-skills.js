64621ab94be0eda7ad8b45fd7a52fcef57c42405
Maximiliano Montaña <maxi.omr01@gmail.com>
2022-10-22 21:17:46 -0300

    Add db-hard-skills.json to github pages

--- js/json-hard-skills.js
+++ js/json-hard-skills.js
@@ -16,6 +16,6 @@ const html = (res) => {
 jsonBase.createStructure(
   "skills__line",
   "skills__box",
-  "https://github.com/momr01/Oracle-ONE-Challenge-Portafolio/blob/main/db/db-hard-skills.json",
+  "https://momr01.github.io/portfolio-jsonapi/db-hard-skills.json",
   html
 );
